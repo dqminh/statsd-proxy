adc8ca02803f93b3b3653508de27beeed7ac9c6f
Daniel Dao <pair+dan@viki.com>
2012-08-23 16:47:19 +0800

    improve sample javascript client

--- sample_client.js
+++ sample_client.js
@@ -1,39 +1,49 @@
 // # Sample client code for statsd-proxy
 //
 // How to use:
-//    var statsd = new Statsd("app.production");
+//    var statsd = new Statsd("stats.example.com", "app.production");
 //    statsd.increment("test.inc", 0.1);
 //    statsd.decrement("test.dec", 0.1);
 //    statsd.timing("test.timing", 100, 0.1);
 window.Statsd = (function($) {
-  function Statsd(prefix) {
+  function Statsd(host, prefix) {
+    this.host = host;
     this.prefix = prefix;
   }
 
   Statsd.prototype.increment = function(name, sampleRate) {
-    var prefix = this.prefix;
-    $.get('/increment', {
-      name: prefix + "." + name,
-      sampleRate: sampleRate
+    $.ajax({
+      url: this.host + "/increment",
+      crossDomain: true,
+      data: {
+        name: this.prefix + "."  + name,
+        sample_rate: sampleRate
+      }
     });
-  }
+  };
 
-  Statsd.prototype.decrement= function(name, sampleRate) {
-    var prefix = this.prefix;
-    $.get('/decrement', {
-      name: prefix + "." + name,
-      sampleRate: sampleRate
+  Statsd.prototype.decrement = function(name, sampleRate) {
+    $.ajax({
+      url: this.host + "/decrement",
+      crossDomain: true,
+      data: {
+        name: this.prefix + "."  + name,
+        sample_rate: sampleRate
+      }
     });
-  }
+  };
 
-  Statsd.prototype.timing= function(name, value, sampleRate) {
-    var prefix = this.prefix;
-    $.get('/timing', {
-      name: prefix + "." + name,
-      value: value,
-      sampleRate: sampleRate
+  Statsd.prototype.timing = function(name, value, sampleRate) {
+    $.ajax({
+      url: this.host + "/timing",
+      crossDomain: true,
+      data: {
+        name: this.prefix + "."  + name,
+        value: value,
+        sample_rate: sampleRate
+      }
     });
-  }
+  };
 
   return Statsd;
 })(jQuery);
